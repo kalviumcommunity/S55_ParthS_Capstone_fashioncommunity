ac5b16deaac814c2d144266f2995c057ff65c09b
Parthssh <naturallyinsane13@gmail.com>
2024-03-27 11:29:19 +0530

    deployed backend

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/',(req,res)=>{
+    res.send('Server Deployed Successfully')
+})
+
+app.listen(PORT ,()=>{
+    console.log('success')
+})
+
+module.exports = app
