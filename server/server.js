23e1eee8cc0922a8c22b4035774b4a4ad3174690
Parthssh <naturallyinsane13@gmail.com>
2024-03-27 11:37:57 +0530

    minor changes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+require('dotenv').config()
 
 app.get('/',(req,res)=>{
     res.send('Server Deployed Successfully')
 })
 
-app.listen(PORT ,()=>{
+app.listen(process.env.PORT ,()=>{
     console.log('success')
 })
 
